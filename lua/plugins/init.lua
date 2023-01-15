0ac19fd248e81ba0060202ad75dad35863730aba
alpha2phi <alpha2phi@gmail.com>
2023-01-15 20:59:34 +0800

    feat: user interface

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -38,7 +38,7 @@ return {
   {
     "andymass/vim-matchup",
     lazy = false,
-    config = function()
+    init = function()
       vim.g.matchup_matchparen_offscreen = { method = "popup" }
     end,
   },
