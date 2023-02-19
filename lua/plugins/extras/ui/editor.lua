3ee9820d706e970eb48d02a8dea210c7b398da25
alpha2phi <alpha2phi@gmail.com>
2023-02-19 23:02:20 +0800

    feat: pde

--- lua/plugins/extras/ui/editor.lua
+++ lua/plugins/extras/ui/editor.lua
@@ -1,9 +1,9 @@
 return {
   {
     "xiyaowong/nvim-transparent",
-    event = "VeryLazy",
+    cmd = { "TransparentEnable", "TransparentDisable", "TransparentToggle" },
     opts = {
-      enable = true, -- boolean: enable transparent
+      enable = false, -- boolean: enable transparent
       extra_groups = { -- table/string: additional groups that should be cleared
         -- In particular, when you set it to 'all', that means all available groups
 
@@ -18,7 +18,6 @@ return {
       exclude = {}, -- table: groups you don't want to clear
     },
     config = function(_, opts)
-      vim.g.transparent_enabled = true
       require("transparent").setup(opts)
     end,
   },
