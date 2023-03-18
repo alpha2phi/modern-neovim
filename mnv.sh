08e0c700b40b5c6505e3c40235588fa136206afc
alpha2phi <alpha2phi@gmail.com>
2023-03-19 00:01:17 +0800

    refactor: use NVIM_APPNAME

--- mnv.sh
+++ mnv.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/sh
 
-MODERN_NEOVIM=~/.config/modern-neovim
-export MODERN_NEOVIM
-
-alias mnv='XDG_DATA_HOME=$MODERN_NEOVIM/share XDG_CACHE_HOME=$MODERN_NEOVIM XDG_CONFIG_HOME=$MODERN_NEOVIM nvim'
-
-mnv
+# https://github.com/neovim/neovim/pull/22128
+MNV_APP_NAME=modern-neovim
+alias mnv="NVIM_APPNAME=$MNV_APP_NAME nvim"
