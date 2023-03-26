64175b736d0d20c4e3661f5126eb8c409050e6b5
alpha2phi <alpha2phi@gmail.com>
2023-03-26 20:36:01 +0800

    chore: test plugins

--- lua/plugins/extras/pde/notes/init.lua
+++ lua/plugins/extras/pde/notes/init.lua
@@ -3,7 +3,7 @@ return {
   { "folke/twilight.nvim", config = true, cmd = { "Twilight", "TwilightEnable", "TwilightDisable" } },
   { "folke/zen-mode.nvim", config = true, cmd = { "ZenMode" } },
   { "dhruvasagar/vim-table-mode", ft = { "markdown", "org", "norg" } },
-  { "lukas-reineke/headlines.nvim", config = true, ft = { "markdown", "org", "norg" } },
+  { "lukas-reineke/headlines.nvim", config = true, ft = { "markdown", "org", "norg" }, enabled = false },
   {
     "jbyuki/nabla.nvim",
     --stylua: ignore
@@ -17,15 +17,7 @@ return {
   {
     "vim-pandoc/vim-pandoc",
     event = "VeryLazy",
-    opts = { patterns = { "*.org", "*.norg", "*.tex" } },
-    config = function(_, opts)
-      vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile", "BufFilePost" }, {
-        pattern = opts.patterns,
-        callback = function()
-          vim.cmd.runtime [[ftplugin/pandoc.vim]]
-        end,
-      })
-    end,
+    enabled = false,
     dependencies = { "vim-pandoc/vim-pandoc-syntax" },
   },
 }
