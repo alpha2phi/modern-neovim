7a49114e655fb2e18740714a0a619e39900f2783
alpha2phi <alpha2phi@gmail.com>
2023-04-10 23:21:38 +0800

    fix: fix navbuddy

--- lua/plugins/navbuddy.lua
+++ lua/plugins/navbuddy.lua
@@ -11,15 +11,6 @@ return {
     keys = {
       { "<leader>vO", function() require("nvim-navbuddy").open() end, desc = "Code Outline (navbuddy)", },
     },
-    opts = {},
-    config = function()
-      local lsp_utils = require "plugins.lsp.utils"
-      lsp_utils.on_attach(function(client, buffer)
-        if client.name ~= "null-ls" then
-          local navbuddy = require "nvim-navbuddy"
-          navbuddy.attach(client, buffer)
-        end
-      end)
-    end,
+    opts = { lsp = { auto_attach = true } },
   },
 }
