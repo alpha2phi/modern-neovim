59e19444f4fcf3292143bd10df580969a245aa8d
alpha2phi <alpha2phi@gmail.com>
2023-05-06 20:35:14 +0800

    chore: fix loading

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ require "config.options"
 require "config.lazy"
 
 vim.api.nvim_create_autocmd("User", {
+  group = vim.api.nvim_create_augroup("ModernNeovim", { clear = true }),
   pattern = "VeryLazy",
   callback = function()
     require "config.autocmds"
