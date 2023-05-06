3ba63618a059eea626b2186166be0530a2962529
alpha2phi <alpha2phi@gmail.com>
2023-05-06 20:40:24 +0800

    fix: fix startup

--- init.lua
+++ init.lua
@@ -1,12 +1,18 @@
 require "config.options"
 require "config.lazy"
 
-vim.api.nvim_create_autocmd("User", {
-  group = vim.api.nvim_create_augroup("ModernNeovim", { clear = true }),
-  pattern = "VeryLazy",
-  callback = function()
-    require "config.autocmds"
-    require "config.keymaps"
-    require "utils.contextmenu"
-  end,
-})
+if vim.fn.argc(-1) == 0 then
+  vim.api.nvim_create_autocmd("User", {
+    group = vim.api.nvim_create_augroup("ModernNeovim", { clear = true }),
+    pattern = "VeryLazy",
+    callback = function()
+      require "config.autocmds"
+      require "config.keymaps"
+      require "utils.contextmenu"
+    end,
+  })
+else
+  require "config.autocmds"
+  require "config.keymaps"
+  require "utils.contextmenu"
+end
