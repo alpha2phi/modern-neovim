ab9e07baf37f115bc5628ce1d64265888639ec26
alpha2phi <alpha2phi@gmail.com>
2023-05-10 20:42:21 +0800

    fix: Neogit with Noice

--- lua/plugins/vcs/init.lua
+++ lua/plugins/vcs/init.lua
@@ -9,7 +9,7 @@ return {
     cmd = "Neogit",
     opts = {
       integrations = { diffview = true },
-      disable_commit_confirmation = false,
+      disable_commit_confirmation = true,
     },
     keys = {
       { "<leader>gs", "<cmd>Neogit kind=tab<cr>", desc = "Status" },
