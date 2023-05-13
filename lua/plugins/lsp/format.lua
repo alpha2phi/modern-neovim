36b31efc87e3f8a04e2357abefa37cc3a795a82e
alpha2phi <alpha2phi@gmail.com>
2023-05-13 19:39:03 +0800

    chore: lsp

--- lua/plugins/lsp/format.lua
+++ lua/plugins/lsp/format.lua
@@ -10,7 +10,7 @@ end
 function M.format()
   local buf = vim.api.nvim_get_current_buf()
   local ft = vim.bo[buf].filetype
-  local have_nls = #require("null-ls.sources").get_available(ft, "NULL_LS_FORMATTING") > 0
+  local have_nls = package.loaded["null-ls"] and (#require("null-ls.sources").get_available(ft, "NULL_LS_FORMATTING") > 0)
 
   vim.lsp.buf.format {
     bufnr = buf,
@@ -24,6 +24,9 @@ function M.format()
 end
 
 function M.on_attach(client, buf)
+  if client.config and client.config.capabilities and client.config.capabilities.documentFormattingProvider == false then
+    return
+  end
   if client.supports_method "textDocument/formatting" then
     vim.api.nvim_create_autocmd("BufWritePre", {
       group = vim.api.nvim_create_augroup("LspFormat." .. buf, {}),
--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -53,6 +53,7 @@ return {
   },
   {
     "williamboman/mason.nvim",
+    build = ":MasonUpdate",
     cmd = "Mason",
     keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
     opts = {
@@ -62,13 +63,20 @@ return {
       },
     },
     config = function(_, opts)
-      require("mason").setup()
+      require("mason").setup(opts)
       local mr = require "mason-registry"
-      for _, tool in ipairs(opts.ensure_installed) do
-        local p = mr.get_package(tool)
-        if not p:is_installed() then
-          p:install()
+      local function ensure_installed()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
         end
+      end
+      if mr.refresh then
+        mr.refresh(ensure_installed)
+      else
+        ensure_installed()
       end
     end,
   },
