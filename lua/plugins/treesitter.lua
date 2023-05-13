1797a3b0c9b41cfa5874825c2f9181048ee21f69
alpha2phi <alpha2phi@gmail.com>
2023-05-13 15:48:07 +0800

    chore: treesitter setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -109,6 +109,17 @@ return {
       },
     },
     config = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        ---@type table<string, boolean>
+        local added = {}
+        opts.ensure_installed = vim.tbl_filter(function(lang)
+          if added[lang] then
+            return false
+          end
+          added[lang] = true
+          return true
+        end, opts.ensure_installed)
+      end
       require("nvim-treesitter.configs").setup(opts)
     end,
   },
