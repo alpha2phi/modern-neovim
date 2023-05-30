3dacfabc806b7032386ebc0c91069fb8de37a9f0
alpha2phi <alpha2phi@gmail.com>
2023-05-30 11:23:00 +0800

    chore: indent

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,7 +19,7 @@ return {
     opts = {
       -- char = "▏",
       char = "│",
-      filetype_exclude = { "help", "alpha", "dashboard", "NvimTree", "Trouble", "lazy" },
+      filetype_exclude = { "help", "alpha", "dashboard", "NvimTree", "Trouble", "lazy", "mason" },
       show_trailing_blankline_indent = false,
       show_current_context = false,
     },
