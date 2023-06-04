9d88bed69e50011831937b2ba500fe5c8af02dcb
alpha2phi <alpha2phi@gmail.com>
2023-06-04 23:36:06 +0800

    fix: lsp formatting

--- lua/plugins/lsp/format.lua
+++ lua/plugins/lsp/format.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 M.autoformat = true
+M.format_notify = false
 
 function M.toggle()
   M.autoformat = not M.autoformat
@@ -9,35 +10,108 @@ end
 
 function M.format()
   local buf = vim.api.nvim_get_current_buf()
-  local ft = vim.bo[buf].filetype
-  local have_nls = package.loaded["null-ls"] and (#require("null-ls.sources").get_available(ft, "NULL_LS_FORMATTING") > 0)
+
+  local formatters = M.get_formatters(buf)
+  local client_ids = vim.tbl_map(function(client)
+    return client.id
+  end, formatters.active)
+
+  if #client_ids == 0 then
+    return
+  end
+
+  if M.format_notify then
+    M.notify(formatters)
+  end
 
   vim.lsp.buf.format(vim.tbl_deep_extend("force", {
     bufnr = buf,
     filter = function(client)
-      if have_nls then
-        return client.name == "null-ls"
-      end
-      return client.name ~= "null-ls"
+      return vim.tbl_contains(client_ids, client.id)
     end,
-  }, require("plugins.lsp.utils").opts("nvim-lspconfig").format or {}))
+  }, require("base.lsp.utils").opts("nvim-lspconfig").format or {}))
 end
 
-function M.on_attach(client, buf)
+function M.notify(formatters)
+  local lines = { "# Active:" }
+
+  for _, client in ipairs(formatters.active) do
+    local line = "- **" .. client.name .. "**"
+    if client.name == "null-ls" then
+      line = line
+        .. " ("
+        .. table.concat(
+          vim.tbl_map(function(f)
+            return "`" .. f.name .. "`"
+          end, formatters.null_ls),
+          ", "
+        )
+        .. ")"
+    end
+    table.insert(lines, line)
+  end
+
+  if #formatters.available > 0 then
+    table.insert(lines, "")
+    table.insert(lines, "# Disabled:")
+    for _, client in ipairs(formatters.available) do
+      table.insert(lines, "- **" .. client.name .. "**")
+    end
+  end
+
+  vim.notify(table.concat(lines, "\n"), vim.log.levels.INFO, {
+    title = "Formatting",
+    on_open = function(win)
+      vim.api.nvim_win_set_option(win, "conceallevel", 3)
+      vim.api.nvim_win_set_option(win, "spell", false)
+      local buf = vim.api.nvim_win_get_buf(win)
+      vim.treesitter.start(buf, "markdown")
+    end,
+  })
+end
+
+function M.supports_format(client)
   if client.config and client.config.capabilities and client.config.capabilities.documentFormattingProvider == false then
-    return
+    return false
   end
-  if client.supports_method "textDocument/formatting" then
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = vim.api.nvim_create_augroup("LspFormat." .. buf, {}),
-      buffer = buf,
-      callback = function()
-        if M.autoformat then
-          M.format()
-        end
-      end,
-    })
+  return client.supports_method "textDocument/formatting" or client.supports_method "textDocument/rangeFormatting"
+end
+
+function M.get_formatters(bufnr)
+  local ft = vim.bo[bufnr].filetype
+  -- check if we have any null-ls formatters for the current filetype
+  local null_ls = package.loaded["null-ls"] and require("null-ls.sources").get_available(ft, "NULL_LS_FORMATTING") or {}
+
+  local ret = {
+    active = {},
+    available = {},
+    null_ls = null_ls,
+  }
+
+  local clients = vim.lsp.get_active_clients { bufnr = bufnr }
+  for _, client in ipairs(clients) do
+    if M.supports_format(client) then
+      if (#null_ls > 0 and client.name == "null-ls") or #null_ls == 0 then
+        table.insert(ret.active, client)
+      else
+        table.insert(ret.available, client)
+      end
+    end
   end
+
+  return ret
+end
+
+function M.on_attach(_, bufnr)
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group = vim.api.nvim_create_augroup("LspFormat." .. bufnr, {}),
+    buffer = bufnr,
+    callback = function()
+      if M.autoformat then
+        M.format()
+      end
+    end,
+  })
 end
 
 return M
