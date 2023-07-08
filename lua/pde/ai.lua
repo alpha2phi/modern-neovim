18633817e2ae14dbd612273d578a8f0e64f4e97c
alpha2phi <alpha2phi@gmail.com>
2023-07-08 13:33:53 +0800

    feat: AI pde

--- lua/pde/ai.lua
+++ lua/pde/ai.lua
@@ -51,6 +51,7 @@ return {
 
     "Exafunction/codeium.vim",
     event = "InsertEnter",
+    enabled = true,
     -- stylua: ignore
     config = function ()
       vim.g.codeium_disable_bindings = 1
@@ -59,6 +60,38 @@ return {
       vim.keymap.set("i", "<A-b>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
       vim.keymap.set("i", "<A-c>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
       vim.keymap.set("i", "<A-s>", function() return vim.fn["codeium#Complete"]() end, { expr = true })
+    end,
+  },
+  {
+    "huggingface/hfcc.nvim",
+    event = "InsertEnter",
+    enabled = false,
+    opts = {
+      model = "bigcode/starcoder",
+    },
+  },
+  {
+    "codota/tabnine-nvim",
+    enabled = false,
+    build = "./dl_binaries.sh",
+    event = "VeryLazy",
+    config = function()
+      require("tabnine").setup {
+        disable_auto_comment = true,
+        accept_keymap = "<A-l>",
+        dismiss_keymap = "<A-c>",
+        debounce_ms = 800,
+        suggestion_color = { gui = "#808080", cterm = 244 },
+        exclude_filetypes = { "TelescopePrompt" },
+      }
+    end,
+  },
+  {
+    "mthbernardes/codeexplain.nvim",
+    enabled = false,
+    cmd = "CodeExplain",
+    build = function()
+      vim.cmd [[silent UpdateRemotePlugins]]
     end,
   },
   {
