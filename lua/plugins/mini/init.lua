d4b62bea2a1494d6e299a8dc8dbdae725f60fd66
alpha2phi <alpha2phi@gmail.com>
2023-07-09 20:33:02 +0800

    fix: minor changes

--- lua/plugins/mini/init.lua
+++ lua/plugins/mini/init.lua
@@ -213,6 +213,13 @@ return {
   {
     "echasnovski/mini.hipatterns",
     event = "BufReadPre",
-    opts = {},
+    opts = function()
+      local hi = require "mini.hipatterns"
+      return {
+        highlighters = {
+          hex_color = hi.gen_highlighter.hex_color { priority = 2000 },
+        },
+      }
+    end,
   },
 }
