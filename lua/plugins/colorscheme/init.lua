85d6fcde33cc4c77cfee55135a2e44d3a68ee567
alpha2phi <alpha2phi@gmail.com>
2023-07-11 11:10:00 +0800

    chore: colorscheme

--- lua/plugins/colorscheme/init.lua
+++ lua/plugins/colorscheme/init.lua
@@ -29,7 +29,41 @@ return {
       tokyonight.load()
     end,
   },
-  { "catppuccin/nvim", lazy = false, enabled = false, name = "catppuccin" },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        alpha = true,
+        cmp = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        lsp_trouble = true,
+        mason = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        noice = true,
+        notify = true,
+        neotree = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        which_key = true,
+      },
+    },
+  },
   { "rebelot/kanagawa.nvim", lazy = false, name = "kanagawa" },
   {
     "ellisonleao/gruvbox.nvim",
