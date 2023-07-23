1c3b34c1dfdf135f29445e6d2426580c5bbdbf08
alpha2phi <alpha2phi@gmail.com>
2023-07-23 15:39:08 +0800

    chore: telescope

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+M.root_patterns = { ".git", "lua" }
+
 local function default_on_open(term)
   vim.cmd "stopinsert"
   vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
@@ -67,6 +69,41 @@ function M.has(plugin)
   return require("lazy.core.config").plugins[plugin] ~= nil
 end
 
+function M.get_root()
+  ---@type string?
+  local path = vim.api.nvim_buf_get_name(0)
+  path = path ~= "" and vim.loop.fs_realpath(path) or nil
+  ---@type string[]
+  local roots = {}
+  if path then
+    for _, client in pairs(vim.lsp.get_active_clients { bufnr = 0 }) do
+      local workspace = client.config.workspace_folders
+      local paths = workspace and vim.tbl_map(function(ws)
+        return vim.uri_to_fname(ws.uri)
+      end, workspace) or client.config.root_dir and { client.config.root_dir } or {}
+      for _, p in ipairs(paths) do
+        local r = vim.loop.fs_realpath(p)
+        if path:find(r, 1, true) then
+          roots[#roots + 1] = r
+        end
+      end
+    end
+  end
+  table.sort(roots, function(a, b)
+    return #a > #b
+  end)
+  ---@type string?
+  local root = roots[1]
+  if not root then
+    path = path and vim.fs.dirname(path) or vim.loop.cwd()
+    ---@type string?
+    root = vim.fs.find(M.root_patterns, { path = path, upward = true })[1]
+    root = root and vim.fs.dirname(root) or vim.loop.cwd()
+  end
+  ---@cast root string
+  return root
+end
+
 function M.telescope(builtin, opts)
   local params = { builtin = builtin, opts = opts }
   return function()
@@ -91,7 +128,6 @@ function M.telescope(builtin, opts)
         return true
       end
     end
-
     require("telescope.builtin")[builtin](opts)
   end
 end
