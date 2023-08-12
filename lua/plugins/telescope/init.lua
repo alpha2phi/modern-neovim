94338da813a193ba3b9d699f378d9c2bae6fa442
alpha2phi <alpha2phi@gmail.com>
2023-08-12 22:03:23 +0800

    chore: telescope

--- lua/plugins/telescope/init.lua
+++ lua/plugins/telescope/init.lua
@@ -109,11 +109,8 @@ return {
 
       local opts = {
         defaults = {
-          -- prompt_prefix = icons.ui.Telescope .. " ",
-          -- selection_caret = icons.ui.Forward .. " ",
-          -- border = {},
-          -- borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-          -- color_devicons = true,
+          prompt_prefix = icons.ui.Telescope .. " ",
+          selection_caret = icons.ui.Forward .. " ",
           mappings = mappings,
           vimgrep_arguments = {
             "rg",
@@ -125,8 +122,6 @@ return {
             "--column",
             "--smart-case",
           },
-          prompt_prefix = "   ",
-          selection_caret = "  ",
           entry_prefix = "  ",
           initial_mode = "insert",
           selection_strategy = "reset",
@@ -159,22 +154,22 @@ return {
           qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
           buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
         },
-        -- pickers = {
-        --   find_files = {
-        --     theme = "dropdown",
-        --     previewer = false,
-        --     hidden = true,
-        --     find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
-        --   },
-        --   git_files = {
-        --     theme = "dropdown",
-        --     -- previewer = false,
-        --   },
-        --   buffers = {
-        --     theme = "dropdown",
-        --     previewer = false,
-        --   },
-        -- },
+        pickers = {
+          find_files = {
+            theme = "dropdown",
+            previewer = false,
+            hidden = true,
+            find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+          },
+          git_files = {
+            theme = "dropdown",
+            previewer = false,
+          },
+          buffers = {
+            theme = "dropdown",
+            previewer = false,
+          },
+        },
         extensions = {
           file_browser = {
             theme = "dropdown",
@@ -201,21 +196,13 @@ return {
       telescope.load_extension "lazy"
       telescope.load_extension "noice"
       telescope.load_extension "notify"
-      -- telescope.load_extension "macros"
 
       -- Highlights
       local fg_bg = require("utils").fg_bg
-      -- local bg = require("utils").bg
       local colors = require "plugins.colorscheme.colors"
       fg_bg("TelescopePreviewTitle", colors.black, colors.green)
       fg_bg("TelescopePromptTitle", colors.black, colors.red)
       fg_bg("TelescopeResultsTitle", colors.darker_black, colors.blue)
-      -- fg_bg("TelescopeBorder", colors.darker_black, colors.darker_black)
-      -- fg_bg("TelescopePromptBorder", colors.black2, colors.black2)
-      -- fg_bg("TelescopePromptNormal", colors.white, colors.black2)
-      -- fg_bg("TelescopePromptPrefix", colors.red, colors.black2)
-      -- bg("TelescopeNormal", colors.darker_black)
-      -- bg("TelescopeSelection", colors.black2)
     end,
   },
   {
