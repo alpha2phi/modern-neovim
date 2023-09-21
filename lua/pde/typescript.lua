a6b406c1376398f97e35e378362279590391156d
alpha2phi <alpha2phi@gmail.com>
2023-09-21 09:45:17 +0800

    chore: inlay hints

--- lua/pde/typescript.lua
+++ lua/pde/typescript.lua
@@ -18,7 +18,19 @@ return {
   {
     "pmizio/typescript-tools.nvim",
     dependencies = { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
-    opts = {},
+    opts = {
+      tsserver_file_preferences = {
+        -- Inlay Hints
+        includeInlayParameterNameHints = "all",
+        includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+      },
+    },
     config = function(_, opts)
       require("plugins.lsp.utils").on_attach(function(client, bufnr)
         if client.name == "tsserver" then
