07079ba71a27b8bc06d8ef140d6bbfff4f5f5dfd
alpha2phi <alpha2phi@gmail.com>
2023-10-01 19:24:34 +0800

    chore: fixed formatting

--- lua/pde/lua.lua
+++ lua/pde/lua.lua
@@ -17,7 +17,6 @@ return {
   },
   {
     "nvimtools/none-ls.nvim",
-
     opts = function(_, opts)
       local nls = require "null-ls"
       table.insert(opts.sources, nls.builtins.formatting.stylua)
