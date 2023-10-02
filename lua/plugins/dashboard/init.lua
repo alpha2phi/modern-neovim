df104356ecfcb9d82649363cfb543f3e70997636
alpha2phi <alpha2phi@gmail.com>
2023-10-02 22:15:05 +0800

    chore: dashboard

--- lua/plugins/dashboard/init.lua
+++ lua/plugins/dashboard/init.lua
@@ -5,14 +5,15 @@ return {
     local dashboard = require "alpha.themes.dashboard"
     dashboard.section.header.val = require("plugins.dashboard.logo")["random"]
     dashboard.section.buttons.val = {
-      dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-      dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-      dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
       dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-      dashboard.button("s", "勒" .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
+      dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+      dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
       dashboard.button("l", "鈴" .. " Lazy", ":Lazy<CR>"),
+      dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("p", " " .. " Projects", ":Telescope projects <CR>"),
       dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+      dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+      dashboard.button("s", "勒" .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
     }
     for _, button in ipairs(dashboard.section.buttons.val) do
       button.opts.hl = "AlphaButtons"
