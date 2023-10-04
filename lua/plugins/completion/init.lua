6cacfa2d1cb92f5057672e8504f2c524e6e388ab
alpha2phi <alpha2phi@gmail.com>
2023-10-04 10:28:47 +0800

    chore: update completion settings

--- lua/plugins/completion/init.lua
+++ lua/plugins/completion/init.lua
@@ -115,9 +115,9 @@ return {
           }),
         },
         sources = cmp.config.sources {
-          { name = "nvim_lsp", group_index = 1 },
+          { name = "nvim_lsp", group_index = 1, max_item_count = 15 },
           { name = "codeium", group_index = 1 },
-          { name = "luasnip", group_index = 1 },
+          { name = "luasnip", group_index = 1, max_item_count = 8 },
           { name = "buffer", group_index = 2 },
           { name = "path", group_index = 2 },
           { name = "git", group_index = 2 },
