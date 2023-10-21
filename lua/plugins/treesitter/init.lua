df5a594edd1baaf28087101206704543039584dc
alpha2phi <alpha2phi@gmail.com>
2023-10-21 15:09:31 +0800

    chore: treesitter movements

--- lua/plugins/treesitter/init.lua
+++ lua/plugins/treesitter/init.lua
@@ -118,6 +118,18 @@ return {
         end, opts.ensure_installed)
       end
       require("nvim-treesitter.configs").setup(opts)
+
+      -- local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+
+      -- -- vim way: ; goes to the direction you were moving.
+      -- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+      -- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+
+      -- -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+      -- vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+      -- vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+      -- vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+      -- vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
     end,
   },
   {
